650c6f7f5dc1bd99f4502a9406e00956d3e7d813
Cyrino808 <134535950+Cyrino808@users.noreply.github.com>
2023-05-24 14:57:29 -0300

    Update passaport_config.js

--- auth/passaport_config.js
+++ auth/passaport_config.js
@@ -3,8 +3,8 @@ const GoogleStrategy = require("passport-google-oauth2").Strategy;
 const User = require("../Models/User");
 
         passport.use(new GoogleStrategy({
-            clientID: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            clientID: process.env.CLIENT_ID,
+            clientSecret: process.env.LIENT_SECRET,
             callbackURL: "http://localhost:8000/auth/google/callback",
             passReqToCallback : true
           },
